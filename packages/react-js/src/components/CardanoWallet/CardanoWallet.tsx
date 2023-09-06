5dee6d9ea8bb7dbdddb72b24aa75352e6563b618
Jaylan Snelson <jaylan210@gmail.com>
2023-09-05 19:37:48 -0500

    Adding react and storybook. Default configs.

---
+++ packages/react-js/src/components/CardanoWallet/CardanoWallet.tsx
@@ -0,0 +1,5 @@
+export default function CardanoWallet() {
+  return (
+    <div>CardanoWallet</div>
+  )
+}
---
+++ packages/react-js/src/stories/CardanoWallet.stories.tsx
@@ -0,0 +1,12 @@
+import CardanoWallet from "../components/CardanoWallet/CardanoWallet";
+
+export default {
+    title: 'Cardano Wallet',
+    component: CardanoWallet
+}
+
+const Template = () => (
+    <CardanoWallet />
+)
+
+export const Primary = Template.bind({})
